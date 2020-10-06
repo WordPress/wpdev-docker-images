058e77618ed415d8a3ce31ff7e9122183c05172e
Jon Desrosiers <desrosj@users.noreply.github.com>
2020-10-06 12:06:00 -0400

    PHP 8 Docker Updates (#36)
    * Update the PHP8 image to contain the same packages and extensions as previous versions of PHP.
    
    * Remove PECL extensions.
    
    * update Docker file after last commit.
    
    * Attempt to use the standard PHP Docker file.
    
    * Use the beta4 version from the official PHP Docker.
    
    * Attempt to use `pickle` to replace PECL.
    
    * Try 2
    
    * Imagick does not yet support PHP 8.
    
    * Update PHP 8 Dockerfile after last change
    
    * Use the PHP 8.0.0rc1 image released today.
    
    * Update the `version_compare()` calls targeting >= PHP 8.0.
    
    * Attempt to install Imagick from `master`.
    
    * Revert "Attempt to install Imagick from `master`."
    
    This reverts commit 3ceb909a508ac8b146ffdedbe4c98a5098decca7.

--- update.php
+++ update.php
@@ -176,10 +176,10 @@ $php_versions = array(
 	),
 	'8.0' => array(
 		'php' => array(
-			'base_name'       => 'devilbox/php-fpm-8.0:latest',
-			'apt'             => array( 'unzip', 'sudo', 'rsync' ),
-			'extensions'      => array( 'mysqli' ),
-			'pecl_extensions' => array(),
+			'base_name'       => 'php:8.0.0rc1-fpm',
+			'apt'             => array( 'libjpeg-dev', 'libpng-dev', 'libzip-dev', 'libmemcached-dev', 'unzip', 'libmagickwand-dev', 'ghostscript', 'libonig-dev', 'locales', 'sudo', 'rsync', 'libxslt-dev' ),
+			'extensions'      => array( 'gd', 'opcache', 'mysqli', 'zip', 'exif', 'intl', 'mbstring', 'xml', 'xsl' ),
+			'pecl_extensions' => array( 'memcached-3.1.5' ),
 			'composer'        => true,
 		),
 		'phpunit' => 9,
@@ -319,12 +319,22 @@ foreach ( $php_versions as $version => $images ) {
 
 				if ( $config['pecl_extensions'] ) {
 					$install_extensions .= " \\\n\t\\\n";
-					$install_extensions .= array_reduce( $config['pecl_extensions'], function ( $command, $extension ) {
+
+					if ( version_compare( $version, '7.4', '>' ) === true ) {
+						$install_extensions .= "\tcurl --location --output /usr/local/bin/pickle https://github.com/FriendsOfPHP/pickle/releases/download/v0.6.0/pickle.phar; \\\n";
+						$install_extensions .= "\tchmod +x /usr/local/bin/pickle; \\\n\t\\\n";
+					}
+
+					$install_extensions .= array_reduce( $config['pecl_extensions'], function ( $command, $extension ) use ( $version ) {
 						if ( $command ) {
 							$command .= " \\\n";
 						}
 
-						$command .= "\tpecl install $extension;";
+						if ( version_compare( $version, '7.4', '>' ) === true ) {
+							$command .= "\tpickle install $extension;";
+						} else {
+							$command .= "\tpecl install $extension;";
+						}
 
 						if ( 0 === strpos( $extension, 'imagick' ) ) {
 							$command .= " \\\n\tdocker-php-ext-enable imagick;";
