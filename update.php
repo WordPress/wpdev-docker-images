008e1714fa330b508cd0c8b9b0b6bff6c4989574
Jon Desrosiers <desrosj@users.noreply.github.com>
2020-12-01 11:31:27 -0500

    Add missing extensions to PHP 5.3 (#49)
    * Add missing extensions to PHP5.3.

--- update.php
+++ update.php
@@ -37,8 +37,8 @@ $php_versions = array(
 	'5.3' => array(
 		'php' => array(
 			'base_name'       => 'devilbox/php-fpm-5.3:latest',
-			'apt'             => array( 'unzip', 'sudo', 'rsync' ),
-			'extensions'      => array(),
+			'apt'             => array( 'libjpeg-dev', 'libpng-dev', 'libzip-dev', 'libmemcached-dev', 'unzip', 'libmagickwand-dev', 'ghostscript', 'libicu-dev', 'libonig-dev', 'locales', 'sudo', 'rsync', 'libxslt-dev' ),
+			'extensions'      => array( 'gd', 'mysql', 'mysqli', 'zip', 'exif', 'intl', 'mbstring', 'xml', 'xsl' ),
 			'pecl_extensions' => array( 'zendopcache-7.0.5', 'xdebug-2.2.7' ),
 			'composer'        => true,
 		),
