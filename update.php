d6d6d0f7b54ea7fdb3a9e690cda8d9e7c8041bc9
Jonathan Desrosiers <359867+desrosj@users.noreply.github.com>
2026-05-15 11:56:54 -0400

    Add xDebug to the PHP 8.5 image.

--- update.php
+++ update.php
@@ -144,7 +144,7 @@ $php_versions = array(
 			'base_name'       => 'php:8.5-fpm',
 			'apt'             => array( 'libjpeg-dev', 'libpng-dev', 'libwebp-dev', 'libavif-dev', 'libaom-dev', 'libdav1d-dev', 'libheif-dev', 'libzip-dev', 'libssl-dev', 'libmemcached-dev', 'unzip', 'libmagickwand-dev', 'ghostscript', 'libonig-dev', 'locales', 'sudo', 'rsync' ),
 			'extensions'      => array( 'gd', 'mysqli', 'zip', 'exif', 'intl', 'mbstring' ),
-			'pecl_extensions' => array( 'memcached-3.4.0', 'imagick' ),
+			'pecl_extensions' => array( 'memcached-3.4.0', 'imagick', 'xdebug-3.5.1' ),
 			'composer'        => true,
 		),
 		'phpunit' => 9,
