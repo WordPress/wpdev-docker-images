7d11b80bbfa1ba7f8eb79b98c86c071a271eb356
Jonathan Desrosiers <359867+desrosj@users.noreply.github.com>
2026-08-17 10:41:19 -0700

    Remove all PECL extentions.

--- update.php
+++ update.php
@@ -121,7 +121,7 @@ $php_versions = array(
             'base_name'       => 'php:8.6-rc-fpm',
             'apt'             => array( 'libjpeg-dev', 'libpng-dev', 'libwebp-dev', 'libavif-dev', 'libaom-dev', 'libdav1d-dev', 'libheif-dev', 'libzip-dev', 'libssl-dev', 'libmemcached-dev', 'unzip', 'libmagickwand-dev', 'ghostscript', 'libonig-dev', 'locales', 'sudo', 'rsync' ),
             'extensions'      => array( 'gd', 'mysqli', 'zip', 'exif', 'intl', 'mbstring' ),
-            'pecl_extensions' => array( 'memcached-3.4.0', 'imagick' ),
+            'pecl_extensions' => array(),
             'composer'        => true,
         ),
         'cli' => array(
